f2b19e2333c112617b96d31b588fd347ca0b8728
sajjadsadiq <sajjadsadiq2020@gmail.com>
2022-11-10 11:19:31 +0600

    react routes setup and pages create

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import ForgotPassword from '../pages/ForgotPassword'
+import Home from '../pages/Home'
+import Offers from '../pages/Offers'
+import SingIn from '../pages/SingIn'
+import SingUp from '../pages/SingUp'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/offers" element={<Offers/>}/>
+        <Route path="/singin" element={<SingIn/>}/>
+        <Route path="/singup" element={<SingUp/>}/>
+        <Route path="/forgotpassword" element={<ForgotPassword/>}/>
+      </Routes>
+    </>
   )
 }
 
